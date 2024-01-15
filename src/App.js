eb7d90d670533b0f82214d7cdcc6593bb9875293
jetsada437 <s6503052412170@email.kmutnb.ac.th>
2024-01-15 11:30:12 +0700

    EventData2 in function component
    EventData2 in function component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Button from './class-components';
 import { Calculator2 } from './calculator';
+import { EventData2 } from './event-data';
 
 function App() {
-  return <Calculator2/>
+  return <EventData2/>
 }
 
 export default App;
