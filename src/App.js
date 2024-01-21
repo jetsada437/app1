affe55a6d3da7a793b46e4fe18b90dc771f93555
jetsada437 <152828016+jetsada437@users.noreply.github.com>
2024-01-21 20:24:01 +0700

    Add refs-func
    Add refs-func

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Button from './class-components';
-import { Calculator2 } from './calculator';
-import { EventData2 } from './event-data';
-
+import RefsFunc from './refs-func';
 function App() {
-  return <EventData2/>
+  return <RefsFunc/>
 }
 
 export default App;
