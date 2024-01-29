51ae1005af79a7ef1cbcda827db2b28200344c82
jetsada437 <s6503052412170@email.kmutnb.ac.th>
2024-01-29 11:34:07 +0700

    Function Header2&Content2
    Function Header2&Content2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { userContext } from './context';
-import Content from './context-content';
+import Header2 from './context-header2';
+import Content2 from './context-content2';
 
 function App() {
-  return (
-    <userContext.Provider value={'Tom & Sun'}>
-      <Content/>
+  let[user,setUser] = React.useState('')
+  return(
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
     </userContext.Provider>
   )
 }
