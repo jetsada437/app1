39b262e5d43e7df531733b75095d2d97ddf9911a
jetsada437 <s6503052412170@email.kmutnb.ac.th>
2024-03-04 10:38:59 +0700

    create form-check.js
    create form-check.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import FormText from './form-text';
+import Formcheck from './form-check';
 
 function App() {
-  return <FormText/>
+  return <Formcheck/>
 }
 export default App
